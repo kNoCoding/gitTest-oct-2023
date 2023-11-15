e60fa2507a95426dd9b2b38e9e2df38712ff06f7
Konstantin Nochevny <kosnobiz@gmail.com>
2023-11-15 22:29:23 +0200

    Add getRandomColor() to util.js

--- js/util.js
+++ js/util.js
@@ -6,3 +6,11 @@ function getRandomInt(min, max) {
     return res
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
